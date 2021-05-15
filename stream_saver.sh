ac466848b65eb9521ea2910dda4a50974c875087
Arnaud <arrnicel@gmail.com>
2021-05-15 17:13:25 +0200

    Add files via upload

---
+++ stream_saver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /tmp
+while true
+do
+    streamer_name="???????"
+	timestamp=$(date +%s)
+    timedate=$(date)
+	streamlink twitch.tv/$streamer_name best --twitch-disable-hosting -o ${streamer_name}_$timestamp.mp4 > /dev/null
+	(youtube-upload --title="${streamer_name^} Live $timestamp $timedate" --privacy=unlisted --playlist="Live SAUVEGARDE" ${streamer_name}_$timestamp.mp4 > /dev/null 2>&1; if test -f "${streamer_name}_$timestamp.mp4"; then echo "Fichier ${streamer_name}_$timestamp.mp4 envoyé"; fi; rm ${streamer_name}_$timestamp.mp4 > /dev/null)&
+	sleep 1m
+done
