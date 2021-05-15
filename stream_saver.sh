c324ac79302593aa482d7d5045a3bc0d5651953f
Arnaud <arrnicel@gmail.com>
2021-05-15 17:14:55 +0200

    Update stream_saver.sh

--- stream_saver.sh
+++ stream_saver.sh
@@ -2,9 +2,9 @@
 cd /tmp
 while true
 do
-    streamer_name="???????"
+	streamer_name="???????"
 	timestamp=$(date +%s)
-    timedate=$(date)
+	timedate=$(date)
 	streamlink twitch.tv/$streamer_name best --twitch-disable-hosting -o ${streamer_name}_$timestamp.mp4 > /dev/null
 	(youtube-upload --title="${streamer_name^} Live $timestamp $timedate" --privacy=unlisted --playlist="Live SAUVEGARDE" ${streamer_name}_$timestamp.mp4 > /dev/null 2>&1; if test -f "${streamer_name}_$timestamp.mp4"; then echo "Fichier ${streamer_name}_$timestamp.mp4 envoyé"; fi; rm ${streamer_name}_$timestamp.mp4 > /dev/null)&
 	sleep 1m
