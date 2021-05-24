5b796192f9e5ec18bc1d65c2ab0cec7bf7c09fbb
Arnaud <arrnicel@gmail.com>
2021-05-24 21:55:35 +0200

    Modif

--- autoTwitchToYouTube.sh
+++ autoTwitchToYouTube.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Move to tmp folder, only a simple "input" file will be created
-cd /tmp
-
 # Every minute, try to download the Twitch stream, and send it to YouTube.
 # Everything through pipe, no video file created
 while true
@@ -10,14 +7,13 @@ do
 
 	# Define some variables
 	streamer_name=$ATY_TWITCH_USER
-	timestamp=$(date +%s)
 	timedate=$(date)
 
 	# Create the input file. Contains upload parameters
-	echo '{"title":"'"${streamer_name^}"' Live - '"$timedate"'","privacyStatus":"unlisted","playlistTitles":["Live SAUVEGARDE"]}' > input
+	echo '{"title":"'"${streamer_name^}"' Live - '"$timedate"'","privacyStatus":"unlisted","playlistTitles":["Live SAUVEGARDE"]}' > /tmp/input.$streamer_name
 
 	# Start streamlink and youtubeuploader app
-	streamlink twitch.tv/$streamer_name best -O 2>/dev/null | youtubeuploader -metaJSON input -filename - >/dev/null 2>&1
+	streamlink twitch.tv/$streamer_name best -O 2>/dev/null | youtubeuploader -metaJSON /tmp/input.$streamer_name -filename - >/dev/null 2>&1
 
 	sleep 1m
 done
